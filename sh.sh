e563d350d5e6de9a06dd043e238bd6a82b7a7af4
g0dm0de1337 <93085327+g0dm0de1337@users.noreply.github.com>
2022-03-24 19:00:15 +0100

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,123 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .GitHub.com/g0dm0de1337
+echo .
+echo .
+echo .
+echo .
+echo .Payload.bin 2 Boot img ..perfekt4Magisk
+sleep 3
+echo .
+echo .
+echo -create a folder with "Boot" as name
+echo .
+echo -unzip your OTA File &
+echo .
+echo -paste your payload.bin in your "Boot" folder
+sleep 5
+echo .
+echo .
+echo .if your file is not in the folder yet,
+echo .break off this script with crtl+c !!
+sleep 5
+echo .
+echo .
+echo .
+echo .have you finished everything ??
+sleep 2
+echo .
+echo .SURE ??
+sleep 3
+echo .then wait 5 seconds until we start
+echo .
+echo .
+echo .
+sleep 5
+echo .
+echo . 
+echo .Allow the next Termux Pop-up !!
+
+cd $HOME
+termux-setup-storage
+sleep 4
+ls
+sleep 3
+clear
+
+
+pkg up -y
+pkg install git -y
+pkg install python -y
+git clone https://GitHub.com/g0dm0de1337/termux_dumper
+
+cd /sdcard/Boot
+cp payload.bin $HOME/termux_dumper
+clear
+echo .
+echo .
+sleep 7
+cd $HOME
+cd termux_dumper
+chmod +x *
+ls
+sleep 3
+pip install --upgrade pip
+pip install protobuf
+pip install -r requirements.txt
+
+
+clear
+
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .GitHub.com/g0dm0de1337
+echo .
+
+
+
+echo .
+echo .
+echo .
+python --version
+sleep 3
+
+echo .copy the next lines, and paste it here 
+echo ENTER
+sleep 7
+clear
+
+echo .
+echo .
+echo .
+echo  cd $HOME/termux_payload
+echo .
+echo .
+echo  python payload_dumper.py payload.bin
+sleep 3
+echo .
+echo .wait for done..
+echo .
+echo .
+sleep 5
+echo .
+echo . 
+echo  cd $HOME/termux_dumper/output
+echo .
+echo .
+echo  cp boot.img /sdcard/Boot/
+echo .
+echo .
+echo .
+sleep 7
+
+exit
